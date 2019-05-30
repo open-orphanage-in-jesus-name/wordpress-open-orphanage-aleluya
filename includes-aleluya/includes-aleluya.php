b64d8b0b9fafb2acfafc14451f72dad37159e86a
loveJesus <loveJesus@loveJesus.xyz>
2019-05-30 23:37:48 +0200

    Hallelujah, remove dependency on Basic Auth Plugin, and CPNHaiti direct things for now WIP

--- includes-aleluya/includes-aleluya.php
+++ includes-aleluya/includes-aleluya.php
@@ -4,7 +4,7 @@
 defined( 'ABSPATH' ) or die( 'Jesus Christ is the Lord . ' );
 
 require_once( dirname( __FILE__ ) . '/roles-aleluya.php' );
-
+require_once( dirname( __FILE__ ) . '/basic-auth-aleluya.php' );
 
 /** Admin from filter, thank You Lord Jesus for https://www.wpbeginner.com/plugins/how-to-change-sender-name-in-outgoing-wordpress-email/ */
 // Function to change email address
@@ -19,8 +19,8 @@ function wpb_sender_name_aleluya( $original_email_from ) {
 }
 
 // Hooking up our functions to WordPress filters
-add_filter( 'wp_mail_from', 'wpb_sender_email_aleluya' );
-add_filter( 'wp_mail_from_name', 'wpb_sender_name_aleluya' );
+//add_filter( 'wp_mail_from', 'wpb_sender_email_aleluya' );
+//add_filter( 'wp_mail_from_name', 'wpb_sender_name_aleluya' );
 /** End admin from filter **/
 
 /** Change login logo, thank You Jesus for https://www.wpoptimus.com/637/change-wordpress-logo-signup-admin/ */
@@ -34,4 +34,4 @@ padding-bottom: 30px;
 </style>
  <?php
 }
-add_action( 'login_enqueue_scripts', 'my_login_logo_aleluya' );
+//add_action( 'login_enqueue_scripts', 'my_login_logo_aleluya' );
