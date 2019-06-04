53ea3a0dff0ce942cd96fda0ccc6d6fd892b9435
loveJesus <loveJesus@loveJesus.xyz>
2019-06-04 16:20:43 -0600

    Hallelujah - artwork and svn things

--- mkzip-aleluya.sh
+++ mkzip-aleluya.sh
@@ -1,2 +1,5 @@
 # For God so loved the world, that He gave His only begotten Son, that all who believe in Him should not perish but have everlasting life
- zip -r ../../../open-orphanage-aleluya.zip * --exclude='./node_modules*' --exclude='*.sh' --exclude='README.md'
+zip -r ../../../open-orphanage-aleluya.zip * --exclude='./node_modules*' --exclude='*.sh' --exclude='README.md'
+cd svn-aleluya/trunk
+unzip ../../../../../open-orphanage-aleluya.zip
+mv assets/* ../assets
